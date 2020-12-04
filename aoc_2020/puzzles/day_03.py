ae9f794015b9c0589c71a69681b840bddef56a3c
Jon Pincott <jon.pincott1@nhs.net>
2020-12-04 15:55:55 +0000

    Refactor day 3

--- aoc_2020/puzzles/day_03.py
+++ aoc_2020/puzzles/day_03.py
@@ -6,26 +6,22 @@ from aoc_2020.utils.io import stream_lines
 
 
 def get_input():
-    return [[col for col in row.strip()] for row in stream_lines(3)]
+    return [row.strip() for row in stream_lines(3)]
 
 
 def count_trees(grid, row_step, col_step):
     rows, cols, row, col, trees = len(grid), len(grid[0]), 0, 0, 0
     while row < rows:
-        trees += grid[row][col] == '#'
+        trees += grid[row][col % cols] == '#'
         row += row_step
         col += col_step
-        col %= cols
     return trees
 
 
 def main():
     grid = get_input()
-    pt1 = count_trees(grid, 1, 3)
-    print(pt1)
-
-    pt2 = prod(starmap(partial(count_trees, grid), [(1, 3), (1, 1), (1, 5), (1, 7), (2, 1)]))
-    print(pt2)
+    print(count_trees(grid, 1, 3))
+    print(prod(starmap(partial(count_trees, grid), [(1, 3), (1, 1), (1, 5), (1, 7), (2, 1)])))
 
 
 if __name__ == '__main__':
