c43d0a2382fb32d27f4e6d47fb52fe7da84a62c4
Jon Pincott <jon.pincott1@nhs.net>
2020-12-05 08:17:36 +0000

    Refactor day 2

--- aoc_2020/puzzles/day_02.py
+++ aoc_2020/puzzles/day_02.py
@@ -1,4 +1,3 @@
-from collections import Counter
 from functools import reduce
 
 from parse import parse
@@ -7,16 +6,13 @@ from aoc_2020.utils.io import stream_lines
 
 
 def main():
-    pt1 = lambda min, max, char, string: Counter(string)[char] in range(min, max + 1)
+    pt1 = lambda min, max, char, string: string.count(char) in range(min, max + 1)
     pt2 = lambda i, j, char, string: (string[i - 1] == char) ^ (string[j - 1] == char)
-
-    results = reduce(
+    print(*reduce(
         lambda acc, args: map(sum, zip(acc, (pt1(*args), pt2(*args)))),
         (parse("{:d}-{:d} {}: {}", line).fixed for line in stream_lines(day=2)),
         (0, 0)
-    )
-
-    print(*results)
+    ))
 
 
 if __name__ == '__main__':
