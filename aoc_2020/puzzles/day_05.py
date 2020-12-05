622c763d83f0112e4fc73d464a2171be7f6d100f
Jon Pincott <jon.pincott1@nhs.net>
2020-12-05 08:18:02 +0000

    Day 5

---
+++ aoc_2020/puzzles/day_05.py
@@ -0,0 +1,26 @@
+from aoc_2020.utils.io import stream_lines
+
+
+def calc_id(line):
+    binstr = line.translate(str.maketrans('FBRL', '0110'))
+    row = int(binstr[:7], 2)
+    col = int(binstr[7:], 2)
+    return 8 * row + col
+
+
+def find_missing(passes):
+    for n in range(max(passes), 0, -1):
+        if n not in passes:
+            return n
+
+
+def main():
+    passes = {calc_id(l) for l in stream_lines(day=5)}
+    max_val = max(passes)
+    print(max_val)
+    missing = find_missing(passes)
+    print(missing)
+
+
+if __name__ == '__main__':
+    main()
