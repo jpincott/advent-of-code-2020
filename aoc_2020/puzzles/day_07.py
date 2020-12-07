3f6d6cf08ae1939c3a72d25996749e03aee27713
Jon Pincott <jon.pincott1@nhs.net>
2020-12-07 10:22:34 +0000

    Day 7

---
+++ aoc_2020/puzzles/day_07.py
@@ -0,0 +1,28 @@
+from re import findall
+
+from aoc_2020.utils.io import stream_lines
+
+
+def get_rules():
+    return {
+        outer: {inner: int(count) for count, inner in findall(r'(?:(\d+) (\w+ \w+))', contents)}
+        for outer, contents in [line.split(' bags contain ') for line in stream_lines(day=7)]
+    }
+
+
+def can_contain_bag(rules, bag, outer):
+    return bag in (inners := rules[outer]) or any(can_contain_bag(rules, bag, inner) for inner in inners)
+
+
+def count_nested_bags(rules, colour):
+    return sum(count * (1 + count_nested_bags(rules, inner)) for inner, count in rules[colour].items())
+
+
+def main():
+    rules = get_rules()
+    print(sum(can_contain_bag(rules, 'shiny gold', bag) for bag in rules.keys()))
+    print(count_nested_bags(rules, 'shiny gold'))
+
+
+if __name__ == '__main__':
+    main()
