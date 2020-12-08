e56ae544b713cbe1eb2a4b6a349f6a4bc0a2b746
Jon Pincott <jon.pincott1@nhs.net>
2020-12-08 09:30:18 +0000

    Refactor day 5

--- aoc_2020/puzzles/day_05.py
+++ aoc_2020/puzzles/day_05.py
@@ -2,22 +2,13 @@ from aoc_2020.utils.io import stream_lines
 
 
 def calc_id(line):
-    binstr = line.translate(str.maketrans('FBRL', '0110'))
-    return int(binstr, 2)
-
-
-def find_missing(passes):
-    for n in range(max(passes), 0, -1):
-        if n not in passes:
-            return n
+    return int(line.translate(str.maketrans('FBRL', '0110')), 2)
 
 
 def main():
-    passes = {calc_id(l) for l in stream_lines(day=5)}
-    max_val = max(passes)
-    print(max_val)
-    missing = find_missing(passes)
-    print(missing)
+    passes = {calc_id(line) for line in stream_lines(day=5)}
+    print(m := max(passes))
+    print(max(set(range(m)) - passes))
 
 
 if __name__ == '__main__':
