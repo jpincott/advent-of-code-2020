eed8ff5d6c70ca01b6813aa8a5a97448c8e2c07a
Jon Pincott <jon.pincott1@nhs.net>
2020-12-10 22:24:22 +0000

    Refactory day 9

--- aoc_2020/puzzles/day_09.py
+++ aoc_2020/puzzles/day_09.py
@@ -7,24 +7,24 @@ from aoc_2020.utils.io import stream_lines
 
 def main():
     nums = [int(n) for n in stream_lines(day=9)]
-    p = 25
+    window = 25
 
-    n = next(
-        nums[p + i]
-        for i, w in enumerate(windowed(nums, p))
-        if all(x + y != nums[i + p] for x, y in combinations(w, 2))
+    pt1 = next(
+        nums[window + i]
+        for i, w in enumerate(windowed(nums, window))
+        if all(x + y != nums[i + window] for x, y in combinations(w, 2))
     )
 
-    print(n)
+    print(pt1)
 
-    m = next(
+    pt2 = next(
         min(w) + max(w)
         for l in range(2, len(nums))
         for w in windowed(nums, l)
-        if sum(w) == n
+        if sum(w) == pt1
     )
 
-    print(m)
+    print(pt2)
 
 
 if __name__ == '__main__':
