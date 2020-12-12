37cf0a655ac873d9345d39251084fcc472cabe60
Jon Pincott <jon.pincott1@nhs.net>
2020-12-12 13:43:13 +0000

    Refactor day 12

--- aoc_2020/puzzles/day_12.py
+++ aoc_2020/puzzles/day_12.py
@@ -1,37 +1,39 @@
 from functools import reduce
 
-from aoc_2020.utils.decorators import timer
 from aoc_2020.utils.io import stream_lines
 
 
-@timer
 def main():
     actions = [(line[0], int(line[1:])) for line in stream_lines(day=12)]
 
     # pt 1
     fns = {
-        'F': lambda x, v, d, n: (x + n * v, v),
-        'N': lambda x, v, d, n: (x + n * 1j, v),
-        'S': lambda x, v, d, n: (x - n * 1j, v),
-        'E': lambda x, v, d, n: (x + n, v),
-        'W': lambda x, v, d, n: (x - n, v),
-        'R': lambda x, v, d, n: (x, v * pow(-1j, n // 90)),
-        'L': lambda x, v, d, n: (x, v * pow(1j, n // 90))
+        'F': lambda x, v, n: (x + n * v, v),
+        'N': lambda x, v, n: (x + n * 1j, v),
+        'S': lambda x, v, n: (x - n * 1j, v),
+        'E': lambda x, v, n: (x + n, v),
+        'W': lambda x, v, n: (x - n, v),
+        'R': lambda x, v, n: (x, v * pow(-1j, n // 90)),
+        'L': lambda x, v, n: (x, v * pow(1j, n // 90))
     }
 
-    ship, _ = reduce(lambda acc, a: fns[a[0]](*acc, *a), actions, (0, 1))
+    ship, _ = perform_actions(actions, fns, 0, 1)
     print(abs(ship.real) + abs(ship.imag))
 
     # pt2
     fns |= {
-        'N': lambda x, v, i, n: (x, v + n * 1j),
-        'S': lambda x, v, i, n: (x, v - n * 1j),
-        'E': lambda x, v, i, n: (x, v + n),
-        'W': lambda x, v, i, n: (x, v - n),
+        'N': lambda x, v, n: (x, v + n * 1j),
+        'S': lambda x, v, n: (x, v - n * 1j),
+        'E': lambda x, v, n: (x, v + n),
+        'W': lambda x, v, n: (x, v - n),
     }
 
-    ship, _ = reduce(lambda acc, a: fns[a[0]](*acc, *a), actions, (0, 10 + 1j))
+    ship, _ = perform_actions(actions, fns, 0, 10 + 1j)
     print(abs(ship.real) + abs(ship.imag))
+
+
+def perform_actions(actions, functions, start, heading):
+    return reduce(lambda acc, a: functions[a[0]](*acc, a[1]), actions, (start, heading))
 
 
 if __name__ == '__main__':
