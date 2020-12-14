59f6454951ace1d09ae6e14a6940b072a8f29c8a
Jon Pincott <jon.pincott1@nhs.net>
2020-12-14 21:47:24 +0000

    Refactor day 14

--- aoc_2020/puzzles/day_14.py
+++ aoc_2020/puzzles/day_14.py
@@ -4,7 +4,22 @@ from aoc_2020.utils.io import stream_lines
 
 
 def load_program():
-    return [l.strip().split(' = ') for l in stream_lines(day=14)]
+    return [
+        (cmd, arg) if cmd == 'mask' else (int(cmd[4:-1]), int(arg))
+        for cmd, arg in (l.strip().split(' = ') for l in stream_lines(day=14))
+    ]
+
+
+def execute(prg, fn):
+    # functional version is slower:
+    # return reduce(lambda a, t: (t[1], a[1]) if t[0] == 'mask' else (a[0], a[1] | fn(a[0], *t)), prg, (0, {}))[1]
+    mask, mem = 0, {}
+    for cmd, arg in prg:
+        if cmd == 'mask':
+            mask = arg
+        else:
+            mem |= fn(mask, cmd, arg)
+    return mem
 
 
 def apply(mask, val):
@@ -13,7 +28,7 @@ def apply(mask, val):
     return val
 
 
-def stream_addresses(mask, loc):
+def gen_locations(mask, loc):
     loc |= int(mask.replace('X', '0'), 2)
     loc &= int(mask.replace('0', '1').replace('X', '0'), 2)
     return (loc | sum(s) for s in powerset(1 << i for i, c in enumerate(mask[::-1]) if c == 'X'))
@@ -23,32 +38,12 @@ def main():
     prg = load_program()
 
     # pt 1
-    mem = write_masked_value_to_location(prg)
+    mem = execute(prg, lambda mask, loc, val: {loc: apply(mask, val)})
     print(sum(mem.values()))
 
     # pt 2
-    mem = write_value_to_masked_locations(prg)
+    mem = execute(prg, lambda mask, loc, val: {loc: val for loc in gen_locations(mask, loc)})
     print(sum(mem.values()))
-
-
-def write_masked_value_to_location(prg):
-    mask, mem = 0, {}
-    for cmd, arg in prg:
-        if cmd == 'mask':
-            mask = arg
-        else:
-            mem[cmd[4:-1]] = apply(mask, int(arg))
-    return mem
-
-
-def write_value_to_masked_locations(prg):
-    mask, mem = 0, {}
-    for cmd, arg in prg:
-        if cmd == 'mask':
-            mask = arg
-        else:
-            mem |= {addr: int(arg) for addr in stream_addresses(mask, int(cmd[4:-1]))}
-    return mem
 
 
 if __name__ == '__main__':
