0ef2061fbf3d91306e772d9a88df5aab276662c0
Jon Pincott <jon.pincott1@nhs.net>
2020-12-15 23:49:50 +0000

    Replace defaultdict with getter

--- aoc_2020/puzzles/day_15_alternative.py
+++ aoc_2020/puzzles/day_15_alternative.py
@@ -1,11 +1,9 @@
-from collections import defaultdict
-
 from aoc_2020.utils.decorators import timer
 
 
 def get_nums():
     nums = [int(i) for i in '18,11,9,0,5,1'.split(',')]
-    return defaultdict(lambda: 0, {n: i + 1 for i, n in enumerate(nums)}), nums[-1], len(nums)
+    return {n: i + 1 for i, n in enumerate(nums)}, nums[-1], len(nums)
 
 
 def main():
@@ -17,7 +15,7 @@ def main():
 def solve_it(limit):
     nums, last, turns = get_nums()
     for turn in range(turns, limit):
-        nums[last], last = turn, 0 if nums[last] == 0 else turn - nums[last]
+        nums[last], last = turn, 0 if nums.get(last, 0) == 0 else turn - nums[last]
     return last
 
 
