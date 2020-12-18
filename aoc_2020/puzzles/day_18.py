bc4243d0ea25f09a090ee64f3e62de6d77ab18f4
Jon Pincott <jon.pincott1@nhs.net>
2020-12-18 14:42:19 +0000

    Day 18 (pt2)

--- aoc_2020/puzzles/day_18.py
+++ aoc_2020/puzzles/day_18.py
@@ -9,28 +9,59 @@ def get_tokens():
     return (deque(re.findall(r'(\d+|\S)', l)) for l in stream_lines(day=18))
 
 
-def term(t, tokens):
+def term(t, tokens, r):
     if t.isnumeric():
         return int(t)
     if t == '(':
-        return evaluate(tokens)
+        return evaluate(tokens, r)
     return 't'
 
 
-def evaluate(tokens):
-    l = term(tokens.popleft(), tokens)
-    while(tokens):
+def evaluate(tokens, fn):
+    l = term(tokens.popleft(), tokens, fn)
+    expr = deque([l])
+    while (tokens):
         o = tokens.popleft()
         if o == ')': break
-        r = term(tokens.popleft(), tokens)
-        l = {'+': operator.add, '*': operator.mul}[o](l, r)
-    return l
+        r = term(tokens.popleft(), tokens, fn)
+        expr.append(o)
+        expr.append(r)
+
+    return fn(expr)
 
 
 def main():
+    def lr(expr):
+        l = expr.popleft()
+        while expr:
+            o = expr.popleft()
+            r = expr.popleft()
+            l = {'+': operator.add, '*': operator.mul}[o](l, r)
+        return l
 
-    print(sum(map(evaluate, get_tokens())))
+    print(sum(evaluate(t, lr) for t in get_tokens()))
 
+    def am(expr):
+        expr = list(expr)
+        # add first
+        while '+' in expr:
+            i = expr.index('+')
+            a = expr[:i - 1]
+            b = expr[i - 1] + expr[i + 1]
+            c = expr[i + 2:]
+            expr = [*a, b, *c]
+
+        # then multiply
+        while '*' in expr:
+            i = expr.index('*')
+            a = expr[:i - 1]
+            b = expr[i - 1] * expr[i + 1]
+            c = expr[i + 2:]
+            expr = [*a, b, *c]
+
+        return expr[0]
+
+    print(sum(evaluate(t, am) for t in get_tokens()))
 
 
 if __name__ == '__main__':
