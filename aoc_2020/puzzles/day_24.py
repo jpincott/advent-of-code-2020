26324da5bcd81f8b4c242a55890c2ea2acd20ad3
Jon Pincott <jon.pincott1@nhs.net>
2020-12-24 21:46:42 +0000

    Day 24 (pt2)

--- aoc_2020/puzzles/day_24.py
+++ aoc_2020/puzzles/day_24.py
@@ -14,10 +14,49 @@ def to_coords(steps):
     return ne + e, se + e
 
 
+def neighbours(c):
+    return {tuple(map(sum, zip(c, d))) for d in ((1, 0), (1, 1), (0, 1), (-1, 0), (-1, -1), (0, -1))}
+
+
+def evolve(cells):
+    next_gen = set()  # results'
+    boundary = Counter()  # all cells to consider for next gen
+
+    # check all active cells
+    for c in cells:
+        # find all 6 neighbours (excludes self)
+        n = neighbours(c)
+
+        # add empty neighbours to boundary
+        boundary.update(n - cells)
+
+        # count active neighbours
+        s = len(n & cells)
+
+        # propagate to next gen if 1 or 2 neighbours
+        if s in (1, 2):
+            next_gen.add(c)
+
+    # check empty cells
+    # propagate to next gen if 2 neighbours
+    next_gen.update({c for c, v in boundary.items() if v == 2})
+
+    # we're done
+    return next_gen
+
+
 def main():
     input = get_input()
     coords = [to_coords(steps) for steps in input]
+
+    # pt 1
     print(sum(v % 2 for v in Counter(coords).values()))
+
+    # pt 2
+    cells = {c for c, v in Counter(coords).items() if v % 2}
+    for _ in range(100):
+        cells = evolve(cells)
+    print(len(cells))
 
 
 if __name__ == '__main__':
